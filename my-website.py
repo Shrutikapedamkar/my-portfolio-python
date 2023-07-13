0d07750f582ae21695f6dc19bbaafa64a46fd955
Shrutika Pedamkar <47322770+Shrutikapedamkar@users.noreply.github.com>
2023-07-13 14:46:57 +0100

    Update my-website.py

--- my-website.py
+++ my-website.py
@@ -24,7 +24,7 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css("Exercises\style\style.css")
+local_css("style.css")
 
 def typingPrint(text):
     st.title(text)
